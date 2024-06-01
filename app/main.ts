ea6286e4ab80ef5138c3a741ef33eddf2d2b2276
mohits-git <mohitkhatri2805@gmail.com>
2024-06-01 19:10:32 +0530

    feat: parsing 'request-target' and responsing to the request path conditionaly

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,13 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
-    socket.end();
+    socket.on('data', (data) => {
+        const request = data.toString();
+        const path = request.split(' ')[1];
+        const response = path === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
+        socket.write(response);
+        socket.end();
+    })
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
