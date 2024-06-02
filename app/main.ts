9b0d688524b706e384ce28dbbb3cf71eb82c7856
mohits-git <mohitkhatri2805@gmail.com>
2024-06-02 08:42:12 +0530

    feat: implement /files/fileName path to read a file from a directory

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,11 @@
 import * as net from 'net';
+import fs from 'fs';
+import pathNode from 'path';
+
+let dir: null | string = null;
+if (process.argv[2] === '--directory') {
+    dir = process.argv[3];
+}
 
 const server = net.createServer((socket) => {
     socket.on('data', (data: Buffer) => {
@@ -19,6 +26,20 @@ const server = net.createServer((socket) => {
                 const userAgent = userAgentHeader?.split(' ')[1];
                 const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
                 socket.write(response);
+            }
+        }
+        else if (path.startsWith('/files/')) {
+            if (!dir) {
+                socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+            }
+            else {
+                const fileName = path.substring(7);
+                try {
+                    const file = fs.readFileSync(pathNode.join(dir, fileName), 'utf-8');
+                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`);
+                } catch (error) {
+                    socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+                }
             }
         }
         else {
